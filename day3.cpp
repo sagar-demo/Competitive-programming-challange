442b07021c86cf0bded8c8756d2de28c01136e30
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2020-12-11 09:38:47 +0530

    Update day3.cpp

--- day3.cpp
+++ day3.cpp
@@ -1,3 +1,4 @@
+// A chef was cooking something special which required exact 200 ml water but he doesn't have 200 ml cup he has a 500 ml cup and 300 ml cup but that dish required exactly 200 ml water.. can you solve this problem
 #include<iostream>
 using namespace std;
 int main()
