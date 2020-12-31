e9cc359dd2ce8abc90da1feafaceef5aec76b289
Rana  Sagar <64120304+sagar-demo@users.noreply.github.com>
2020-12-31 21:02:23 +0530

    Add files via upload

---
+++ patternNEW.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+using namespace std;
+int main(){
+	char h='\3';
+	cout<< endl << endl << endl <<"\t\t\tFrom: \t PROGRAMMING SAGAR"<<endl <<"\t\t\tTo:\t Name"<<endl<<"\t\t";
+	Sleep(500);
+	Beep(523,500);
+	for (int i=0; i<60; i++)
+	{
+		cout<<"_";
+	}
+	Sleep(500);
+	Beep(523,500);
+	cout<<endl << endl <<"\t\t\t\t  i i i i i"<<endl;
+	Sleep(500);
+	Beep(523,500);
+	cout<<"\t\t\t\t  i i i i i"<<endl;
+	Sleep(500);
+	Beep(523,500);
+	cout<<"\t\t\t\t__i_i_i_i_i__";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t       |"<<h<<"\t  "<<h<<"  |";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t     __|"<<h<<" 2021 "<<h<<"     |__";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t    |"<<h<<"   "<<h<<"   "<<h<<"   "<<h<<"   "<<"   "<<"|";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t   _|"<<h<<" HAPPY "<<h<<"  "<<h<<"       |__";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t  |"<<h<<"   "<<h<<"  NEW YEAR YEAR "<<h<<"  |";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t  |"<<h<<"   "<<h<<"   "<<h<<"   "<<h<<"  "<<h<<" "<<h<<" "<<h<<" "<<h<<" "<<h<<"|";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t  |"<<h<<" "<<h<<"   FAMILY  "<<h<<" "<<h<<"  "<<"  "<<"  "<<" |";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t  |";
+	Sleep(500);
+	Beep(523,500);
+	for (int i=0; i<24; i++){
+	cout<<"_";	
+	}
+	Sleep(500);
+	Beep(523,500);
+	cout<<"|"<< endl<< endl << endl;
+	Sleep(500);
+	Beep(523,500);
+	cout<<"\t\tWHISHING YOU";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t\tHAPPY NEW YEAR";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t\t\tFROM";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t\t|||\t |||";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\t\tMITHILESH ";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\t\tVEDIKA ";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\t\tSONAM";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl <<"\tNAVIN";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl << "NIRANJAN";
+	Sleep(500);
+	Beep(523,500);
+	cout<< endl << "SAGAR";
+	
+}
